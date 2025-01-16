6338e615478f8a67a252f800b86769a26434f8a3
Aman Tuladhar <atuladhar.aman@gmail.com>
2025-01-16 06:59:54 -0600

    Stdin put on eventloop

--- src/main.zig
+++ src/main.zig
@@ -24,27 +24,58 @@ pub fn main() !void {
         .data = &tcp_conn_acceptor,
         .callback = TcpConnectionAcceptor.accept_tcp_connection,
     };
+    const stdin = std.io.getStdIn();
+    std.log.debug("STDIN Handle == {d}", .{stdin.handle});
 
-    const changelist: []const posix.Kevent = &[_]posix.Kevent{.{
+    const changelist: []const posix.Kevent = &[_]posix.Kevent{ .{
         .ident = @intCast(server.stream.handle),
         .filter = std.c.EVFILT_READ,
         .flags = std.c.EV_ADD | std.c.EV_ENABLE,
         .fflags = 0,
         .data = 0,
         .udata = @intFromPtr(&event_data),
-    }};
+    }, .{
+        .ident = @intCast(stdin.handle),
+        .filter = std.c.EVFILT_READ,
+        .flags = std.c.EV_ADD | std.c.EV_ENABLE,
+        .fflags = 0,
+        .data = 0,
+        .udata = 0,
+    } };
     var events: [1]posix.Kevent = undefined;
 
     std.log.info("Server started on :4200", .{});
-    while (true) {
-        const nev = try posix.kevent(kq, changelist, &events, null);
-        std.log.info("Number of event received: {d}", .{nev});
-        std.log.debug("RECV::: {any}", .{events});
-        for (events[0..nev]) |event| {
-            const r_event_data: *EventData = @ptrFromInt(event.udata);
-            r_event_data.callback(r_event_data.data) catch |e| {
-                std.log.err("Something went wrong when calling event callback: {any}", .{e});
-            };
+    main_loop: {
+        while (true) {
+            const nev = try posix.kevent(kq, changelist, &events, null);
+            std.log.info("Number of event received: {d}", .{nev});
+            std.log.debug("RECV::: {any}", .{events});
+            for (events[0..nev]) |event| {
+                if (event.ident == stdin.handle) {
+                    var buf: [10]u8 = undefined;
+                    const read_count = stdin.reader().read(&buf) catch continue;
+                    std.log.debug("readcount = {d}, value = \"{any}\"", .{ read_count, buf });
+                    if (std.mem.eql(u8, "exit", buf[0 .. read_count - 1])) {
+                        std.log.debug("Breaking main loop", .{});
+                        const stdin_clist: []const posix.Kevent = &[_]posix.Kevent{.{
+                            .ident = @intCast(stdin.handle),
+                            .filter = std.c.EVFILT_READ,
+                            .flags = std.c.EV_DELETE | std.c.EV_DISABLE,
+                            .fflags = 0,
+                            .data = 0,
+                            .udata = 0,
+                        }};
+                        var empty_ev: [0]posix.Kevent = undefined;
+                        _ = try posix.kevent(kq, stdin_clist, &empty_ev, null);
+                        break :main_loop;
+                    }
+                    continue;
+                }
+                const r_event_data: *EventData = @ptrFromInt(event.udata);
+                r_event_data.callback(r_event_data.data) catch |e| {
+                    std.log.err("Something went wrong when calling event callback: {any}", .{e});
+                };
+            }
         }
     }
 }
